444dc818f81fc8530c53dba77cff130fdcaeba99
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-07 18:35:14 +0300

    Revert "work?"
    This reverts commit e524f6aecff7c7d04b81eaef6eae39dae16b60d4.

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinMinecraftClient.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinMinecraftClient.java
@@ -1,44 +1,18 @@
 package at.hannibal2.skyhanni.mixins.transformers;
 
 import at.hannibal2.skyhanni.events.render.gui.GuiScreenOpenEvent;
-import at.hannibal2.skyhanni.features.inventory.wardrobe.WardrobeApi;
-import at.hannibal2.skyhanni.utils.compat.WrappedAbstractedContainerScreen;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.InventoryScreen;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Minecraft.class, priority = 1001)
+@Mixin(Minecraft.class)
 public class MixinMinecraftClient {
 
     @Inject(method = "setScreen", at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft;screen:Lnet/minecraft/client/gui/screens/Screen;"))
     private void onSetScreen(Screen screen, CallbackInfo ci) {
         new GuiScreenOpenEvent(screen).post();
-    }
-
-    @WrapOperation(
-        method = "handleKeybinds",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"
-        )
-    )
-    private void replaceScreen(
-        Minecraft client,
-        Screen screen,
-        Operation<Void> original
-    ) {
-        if (true && screen instanceof InventoryScreen) {
-            original.call(client, new InventoryScreen(client.player));
-        } else {
-            original.call(client, screen);
-        }
     }
 }
