5ad4d41a2de7f9b71fcac5ef8b588f85f377d198
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-11 19:11:41 +0300

    pull out renderTime

--- src/main/java/at/hannibal2/skyhanni/features/misc/InvincibilityTimer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/InvincibilityTimer.kt
@@ -8,24 +8,31 @@ import at.hannibal2.skyhanni.events.minecraft.SkyHanniRenderWorldEvent
 import at.hannibal2.skyhanni.features.fishing.SeaCreatureDetectionApi
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.LorenzRarity
+import at.hannibal2.skyhanni.utils.LorenzVec
+import at.hannibal2.skyhanni.utils.ServerTimeMark
 import at.hannibal2.skyhanni.utils.TimeUtils.format
 import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.drawDynamicText
+import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.exactBoundingBoxExtraEntities
 import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object InvincibilityTimer {
+
     private val config get() = SkyHanniMod.feature.misc.invincibilityTimer
 
     private val seaCreatures get() = SeaCreatureDetectionApi.getSeaCreatures()
     private val vanquishers get() = VanquisherApi.getVanquishers()
+
     private val INVINCIBILITY = 5.seconds
 
     @HandleEvent(onlyOnSkyblock = true)
     fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
         if (!isEnabled()) return
+
         if (config.mobTypes.contains(MobType.SEA_CREATURE)) {
             handleSeaCreatureRender(event)
         }
+
         if (config.mobTypes.contains(MobType.VANQUISHER)) {
             handleVanquisherRender(event)
         }
@@ -34,32 +41,59 @@ object InvincibilityTimer {
     private fun handleSeaCreatureRender(event: SkyHanniRenderWorldEvent) {
         for (seaCreature in seaCreatures) {
             if (!seaCreature.exists()) continue
-            if (!seaCreature.rarity.isAtLeast(LorenzRarity.LEGENDARY)) return
+            if (!seaCreature.rarity.isAtLeast(LorenzRarity.LEGENDARY)) continue
+
             val height = seaCreature.aabb?.ysize ?: continue
             val pos = seaCreature.pos?.up(height) ?: continue
-            val time = seaCreature.spawnTime + INVINCIBILITY
-            if (time.passedSince() > 1.seconds) continue
-            val timeLeft = time.timeUntil()
-            event.drawDynamicText(pos, "§b${timeLeft.format(showMilliSeconds = true)}", scaleMultiplier = 1.3)
-            if (seaCreature.isOwn) {
-                event.drawDynamicText(pos.up(0.5), "§aOWN MOB", scaleMultiplier = 1.3)
-            }
+
+            renderTimer(
+                event = event,
+                pos = pos,
+                spawnTime = seaCreature.spawnTime,
+                isOwn = seaCreature.isOwn,
+            )
         }
     }
 
     private fun handleVanquisherRender(event: SkyHanniRenderWorldEvent) {
         for (vanquisher in vanquishers) {
-            val time = vanquisher.spawnTime + INVINCIBILITY
-            if (time.passedSince() > 1.seconds) continue
             val mob = vanquisher.mob
-            val height = mob.baseEntity.eyeHeight
-            // TODO: confirm that this looks correct, TODO: confirm this TODO is needed
-            val pos = vanquisher.mob.getLorenzVec().up(height)
-            val timeLeft = time.timeUntil()
-            event.drawDynamicText(pos, "§b${timeLeft.format(showMilliSeconds = true)}", scaleMultiplier = 1.3)
-            if (vanquisher.isOwn) {
-                event.drawDynamicText(pos.up(0.5), "§aOWN MOB", scaleMultiplier = 1.3)
-            }
+            val height = event.exactBoundingBoxExtraEntities(mob).ysize
+            val pos = mob.getLorenzVec().up(height)
+
+            renderTimer(
+                event = event,
+                pos = pos,
+                spawnTime = vanquisher.spawnTime,
+                isOwn = vanquisher.isOwn,
+            )
+        }
+    }
+
+    private fun renderTimer(
+        event: SkyHanniRenderWorldEvent,
+        pos: LorenzVec,
+        spawnTime: ServerTimeMark,
+        isOwn: Boolean,
+    ) {
+        val invincibilityEnd = spawnTime + INVINCIBILITY
+
+        if (invincibilityEnd.passedSince() > 1.seconds) return
+
+        val timeLeft = invincibilityEnd.timeUntil()
+
+        event.drawDynamicText(
+            pos,
+            "§b${timeLeft.format(showMilliSeconds = true)}",
+            scaleMultiplier = 1.3,
+        )
+
+        if (isOwn) {
+            event.drawDynamicText(
+                pos.up(0.5),
+                "§aOWN MOB",
+                scaleMultiplier = 1.3,
+            )
         }
     }
 
