1d348a50e85319a620f2ae985a57272ce94ab5f0
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-11 19:24:15 +0300

    make a shared data class for them

--- src/main/java/at/hannibal2/skyhanni/features/misc/InvincibilityTimer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/InvincibilityTimer.kt
@@ -5,6 +5,7 @@ import at.hannibal2.skyhanni.api.VanquisherApi
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.features.misc.InvincibilityTimerConfig.MobType
 import at.hannibal2.skyhanni.events.minecraft.SkyHanniRenderWorldEvent
+import at.hannibal2.skyhanni.features.fishing.LivingSeaCreatureData
 import at.hannibal2.skyhanni.features.fishing.SeaCreatureDetectionApi
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.LorenzRarity
@@ -20,76 +21,87 @@ import kotlin.time.Duration.Companion.seconds
 object InvincibilityTimer {
 
     private val config get() = SkyHanniMod.feature.misc.invincibilityTimer
+
+    private val seaCreatures get() = SeaCreatureDetectionApi.getSeaCreatures()
+    private val vanquishers get() = VanquisherApi.getVanquishers()
+
     private val INVINCIBILITY = 5.seconds
+
+    private data class InvincibilityMob(
+        val spawnTime: ServerTimeMark,
+        val pos: LorenzVec,
+        val isOwn: Boolean,
+    )
 
     @HandleEvent(onlyOnSkyblock = true)
     fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
         if (!isEnabled()) return
 
         if (config.mobTypes.contains(MobType.SEA_CREATURE)) {
-            handleSeaCreatureRender(event)
+            for (seaCreature in seaCreatures) {
+                val mob = seaCreature.toInvincibilityMob() ?: continue
+                renderTimer(event, mob)
+            }
         }
 
         if (config.mobTypes.contains(MobType.VANQUISHER)) {
-            handleVanquisherRender(event)
-        }
-    }
-
-    private fun handleSeaCreatureRender(event: SkyHanniRenderWorldEvent) {
-        for (seaCreature in SeaCreatureDetectionApi.getSeaCreatures()) {
-            if (!seaCreature.exists()) continue
-            if (!seaCreature.rarity.isAtLeast(LorenzRarity.LEGENDARY)) continue
-
-            val height = seaCreature.aabb?.ysize ?: continue
-            val pos = seaCreature.pos?.up(height) ?: continue
-
-            renderTimer(
-                event = event,
-                pos = pos,
-                spawnTime = seaCreature.spawnTime,
-                isOwn = seaCreature.isOwn,
-            )
-        }
-    }
-
-    private fun handleVanquisherRender(event: SkyHanniRenderWorldEvent) {
-        for (vanquisher in VanquisherApi.getVanquishers()) {
-            val mob = vanquisher.mob
-            val height = event.exactBoundingBoxExtraEntities(mob).ysize
-            val pos = event.exactLocation(mob).up(height)
-
-            renderTimer(
-                event = event,
-                pos = pos,
-                spawnTime = vanquisher.spawnTime,
-                isOwn = vanquisher.isOwn,
-            )
+            for (vanquisher in vanquishers) {
+                val mob = vanquisher.toInvincibilityMob(event)
+                renderTimer(event, mob)
+            }
         }
     }
 
     private fun renderTimer(
         event: SkyHanniRenderWorldEvent,
-        pos: LorenzVec,
-        spawnTime: ServerTimeMark,
-        isOwn: Boolean,
+        mob: InvincibilityMob,
     ) {
-        val invincibilityEnd = spawnTime + INVINCIBILITY
+        val invincibilityEnd = mob.spawnTime + INVINCIBILITY
+
         if (invincibilityEnd.passedSince() > 1.seconds) return
+
         val timeLeft = invincibilityEnd.timeUntil()
 
         event.drawDynamicText(
-            pos,
+            mob.pos,
             "§b${timeLeft.format(showMilliSeconds = true)}",
             scaleMultiplier = 1.3,
         )
 
-        if (isOwn) {
+        if (mob.isOwn) {
             event.drawDynamicText(
-                pos.up(0.5),
+                mob.pos.up(0.5),
                 "§aOWN MOB",
                 scaleMultiplier = 1.3,
             )
         }
+    }
+
+    private fun LivingSeaCreatureData.toInvincibilityMob(): InvincibilityMob? {
+        if (!exists()) return null
+        if (!rarity.isAtLeast(LorenzRarity.LEGENDARY)) return null
+
+        val height = aabb?.ysize ?: return null
+        val pos = this.pos?.up(height) ?: return null
+
+        return InvincibilityMob(
+            spawnTime = spawnTime,
+            pos = pos,
+            isOwn = isOwn,
+        )
+    }
+
+    private fun VanquisherApi.VanquisherData.toInvincibilityMob(
+        event: SkyHanniRenderWorldEvent,
+    ): InvincibilityMob {
+        val height = event.exactBoundingBoxExtraEntities(mob).ysize
+        val pos = event.exactLocation(mob).up(height)
+
+        return InvincibilityMob(
+            spawnTime = spawnTime,
+            pos = pos,
+            isOwn = isOwn,
+        )
     }
 
     fun isEnabled() = config.enabled && config.mobTypes.isNotEmpty()
