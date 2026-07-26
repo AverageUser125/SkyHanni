2266aa98c0a7370b4bdf96893d7e20f6a896f718
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-26 13:50:17 +0300

    Update CachedItemData.kt

--- src/main/java/at/hannibal2/skyhanni/utils/CachedItemData.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/CachedItemData.kt
@@ -1,5 +1,6 @@
 package at.hannibal2.skyhanni.utils
 
+import at.hannibal2.skyhanni.utils.SafeItemStack
 import at.hannibal2.skyhanni.utils.collection.TimeLimitedCache
 import net.minecraft.nbt.CompoundTag
 import kotlin.time.Duration.Companion.minutes
