46e910ef2c9f39f1fb146b43bbc42f3698941d08
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-20 21:29:45 +0300

    detekt

--- src/main/java/at/hannibal2/skyhanni/config/EnforcedConfigValues.kt
+++ src/main/java/at/hannibal2/skyhanni/config/EnforcedConfigValues.kt
@@ -9,7 +9,6 @@ import at.hannibal2.skyhanni.data.jsonobjects.repo.EnforcedValue
 import at.hannibal2.skyhanni.data.jsonobjects.repo.EnforcedValueData
 import at.hannibal2.skyhanni.events.RepositoryReloadEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.SkyBlockUtils
