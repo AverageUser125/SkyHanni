74794bdc41cf103efad32208ac72c331c42b2d8b
AverageUser125 <96657216+AvrgUser@users.noreply.github.com>
2026-09-26 00:09:52 +0300

    detekt

--- src/main/java/at/hannibal2/skyhanni/utils/OSUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/OSUtils.kt
@@ -171,7 +171,7 @@ object OSUtils {
                 StandardCopyOption.ATOMIC_MOVE,
                 StandardCopyOption.REPLACE_EXISTING
             )
-        } catch(_: AtomicMoveNotSupportedException) {
+        } catch (_: AtomicMoveNotSupportedException) {
             Files.move(
                 source,
                 target,
